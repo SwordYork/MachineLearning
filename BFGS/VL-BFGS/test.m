3b1cd6f70513ab5fc54abe39481fb4bc1496ee76
swordyork <sword.york@gmail.com>
2015-01-13 11:51:15 +0800

    bfgs cvx

--- BFGS/VL-BFGS/test.m
+++ BFGS/VL-BFGS/test.m
@@ -3,7 +3,7 @@ f = (@(X) (exp(X(1,:)-1) + exp(1-X(2,:)) + (X(1,:) - X(2,:)).^2));
 %f = (@(X) (sin(0.5*X(1,:).^2 - 0.25 * X(2,:).^2 + 3) .* cos(2*X(1,:) + 1 - exp(X(2,:))) ))
 %f = (@(X) (1-X(1,:)).^2 + 100 * (X(2,:) - X(1,:).^2).^2);
 
-plot_type = 1;
+plot_type = 0;
 [X, Y] = meshgrid([-2:0.1:2]);
 XX = [reshape(X, 1, numel(X)); reshape(Y, 1, numel(Y))];
 
@@ -55,7 +55,7 @@ for i=2:length(h)
     tmp1 = h(:,i-1);
     tmp2 = h(:,i);
     if plot_type == 0
-        quiver(tmp1(1),tmp1(2),tmp2(1)-tmp1(1),tmp2(2)-tmp1(2), 0, 'b','LineWidth',2)   
+        quiver(tmp1(1),tmp1(2),tmp2(1)-tmp1(1),tmp2(2)-tmp1(2), 0, 'y','LineWidth',2)   
     else
         quiver3(tmp1(1),tmp1(2),(f(tmp1) + 0.5)*1.1, tmp2(1)-tmp1(1),tmp2(2)-tmp1(2), (f(tmp2) - f(tmp1))*1.1 , 0, 'y','LineWidth',3)   
     end
